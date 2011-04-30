63658439c52b4e603cc57f4c0a525694e765ce04
artmax <art.eltyshev@gmail.com>
2011-04-30 20:11:42 +0600

    First commit!

---
+++ main.py
@@ -0,0 +1,26 @@
+﻿from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+import models
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        
+        if user:
+            self.response.out.write('Hello, ' + user.nickname()+'!')
+            self.response.out.write('<br><a href="'+users.create_logout_url(self.request.uri)+'">log out</a>')
+        else:
+            self.response.out.write('Hello, Guest!')
+            self.response.out.write('<br><a href="'+users.create_login_url(self.request.uri)+'">log in</a>')
+            
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,52 @@
+from google.appengine.ext import db
+from google.appengine.ext.db import polymodel
+
+class Group(db.Model):
+    name = db.StringProperty(required=True)
+    info = db.TextProperty()
+
+class File(db.Model):
+    file = db.BlobProperty(required=True)
+    info = db.StringProperty()
+    owner = db.ReferenceProperty(required=True)
+
+class User(polymodel.PolyModel):
+    user = db.UserProperty(required=True)
+    name = db.StringProperty(required=True)
+    surname = db.StringProperty(required=True)
+    birthdate = db.DateProperty()
+    isHeadman = db.BooleanProperty()
+    photo = db.ReferenceProperty(File)
+    info = db.TextProperty()
+    icq = db.StringProperty()
+    skype = db.StringProperty()
+    vk = db.StringProperty()
+    facebook = db.StringProperty()
+    
+class Student(User):
+    group = db.ReferenceProperty(Group,required=True)
+    
+class Teacher(User):
+    position = db.StringProperty(required=True)
+
+class UserMessage(db.Model):
+    from_user = db.ReferenceProperty(User, collection_name='from_user')
+    to_user = db.ReferenceProperty(User, collection_name='to_user')
+    text = db.TextProperty()
+    date_time = db.DateProperty(required=True)
+
+class Topic(db.Model):
+    author = db.ReferenceProperty(User,required=True)
+    title = db.StringProperty(required=True)
+    text = db.TextProperty(required=True)
+
+class TopicMessage(db.Model):
+    user = db.ReferenceProperty(User,collection_name='author',required=True)
+    topic = db.ReferenceProperty(Topic,collection_name='topic',required=True)
+    text = db.TextProperty(required=True)
+    date = db.DateProperty(required=True)
+    
+class News(db.Model):
+    user = db.ReferenceProperty(required=True)
+    text = db.TextProperty(required=True)
+    date = db.DateProperty(required=True)
